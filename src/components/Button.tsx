8efefb8c5fe68b0ccb489f876f69cba419b615bd
achu-bsl <asharudeenalikt@gmail.com>
2023-09-01 07:11:15 +0530

    Implement Business section

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 
-export const Button: FC = () => (
-  <div>
-    Button
-  </div>
-);
+export const Button: FC<{styles: string}> = ({styles}) => (
+<button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}>
+  Get Started
+</button>
+  );
