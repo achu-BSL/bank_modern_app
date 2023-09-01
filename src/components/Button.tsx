0c476acd7bf8be3fb722eed8e33945222518e02e
achu-bsl <asharudeenalikt@gmail.com>
2023-09-01 07:48:12 +0530

    Implement CardDeal section

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,10 @@
-import { FC } from 'react';
+import { FC } from "react";
 
-export const Button: FC<{styles: string}> = ({styles}) => (
-<button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}>
-  Get Started
-</button>
-  );
+export const Button: FC<{ styles: string }> = ({ styles }) => (
+  <button
+    type="button"
+    className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]`}
+  >
+    Get Started
+  </button>
+);
--- src/components/CardDeal.tsx
+++ src/components/CardDeal.tsx
@@ -1,7 +1,26 @@
-import { FC } from 'react';
+import { FC } from "react";
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import { Button } from "./Button";
 
 export const CardDeal: FC = () => (
-  <div>
-    CardDeal
-  </div>
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal
+        <br className="sm:block hidden" /> in few easy steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure,
+        repellendus voluptates nihil commodi quos ratione quisquam ipsa officia
+        maxime ullam ut praesentium neque itaque rem saepe aliquam ex placeat
+        libero?
+      </p>
+      <Button styles="mt-10"/>
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="card" className="w-[100%] h-[100%]"/>
+    </div>
+  </section>
 );
